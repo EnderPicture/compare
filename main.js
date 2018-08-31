de316b2a9f30bf8997ba9b7d504804ddf1015171
EnderPicture <enderpicture@gmail.com>
2018-08-31 00:34:30 -0700

    text box and span creation working

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
-document.addEventListener("keypress", KeyboardEvent => {
-    document.getElementById("type box").innerHTML = KeyboardEvent.key;
-    console.log(KeyboardEvent.key);
-});
+let text = '';
+let words = [];
+let textarea = document.getElementById("textarea");
+let wordsBox = document.getElementById('wordsBox');
+let currentWordIndex = 0;
 
-done = function() {
-    console.log("done yeah");
-}
+
+let subText = function() {
+    text = textarea.value;
+    words = text.split(new RegExp('\\s+'));
+
+    let frag = document.createDocumentFragment();
+
+    for (let i = 0; i < words.length; i++) {
+        let newWord = document.createElement('span');
+        newWord.setAttribute('id','w-'+i);
+        
+        let letters = words[i].split('');
+        for (let j = 0; j < letters.length; j++) {
+            let newLetter = document.createElement('span');
+            newLetter.setAttribute('id','w-'+i+' l-'+j);
+            newLetter.innerHTML = letters[j];
+            newWord.appendChild(newLetter);
+        }
+        frag.appendChild(newWord);
+    }
+
+    wordsBox.appendChild(frag);
+};
+
+
+
+
