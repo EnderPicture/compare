859a1a6e550aedf745cdfe543ceff3b899a0e6e4
EnderPicture <enderpicture@gmail.com>
2018-09-01 01:34:02 -0700

    first working alpha type practice code

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 let text = '';
 let words = [];
-let textarea = document.getElementById("textarea");
+let textarea = document.getElementById('textarea');
+let textBox = document.getElementById('type-box')
 let wordsBox = document.getElementById('wordsBox');
-let currentWordIndex = 0;
+
+let currentWordIndex = -1;
+let currentWordElement;
+let currentLetterIndex = -1;
+let currentLetterElement;
+
+let wordSelected = 'wordSelected';
+let wordNotSelected = 'wordNotSelected';
+let typeError = 'typeError';
+let typeCorrect = 'typeCorrect';
+let typeSelected = 'typeSelected';
+let typeNotSelected = 'typeNotSlected';
 
 
-let subText = function() {
+function subText() {
     text = textarea.value;
     words = text.split(new RegExp('\\s+'));
 
@@ -13,20 +25,67 @@ let subText = function() {
 
     for (let i = 0; i < words.length; i++) {
         let newWord = document.createElement('span');
-        newWord.setAttribute('id','w-'+i);
+        newWord.id = 'w-' + i ;
+        newWord.className = wordNotSelected;
         
-        let letters = words[i].split('');
+        let letters = (words[i]+' ').split('');
         for (let j = 0; j < letters.length; j++) {
             let newLetter = document.createElement('span');
-            newLetter.setAttribute('id','w-'+i+' l-'+j);
+            newLetter.id = 'w-' + i+' l-' + j;
+            newLetter.className = typeNotSelected;
+
             newLetter.innerHTML = letters[j];
             newWord.appendChild(newLetter);
         }
+        
         frag.appendChild(newWord);
     }
 
     wordsBox.appendChild(frag);
+    start();
 };
+
+function start() {
+    incrementWord();
+    incrementLetter();
+    
+}
+
+function incrementWord() {
+    currentWordIndex++;
+    currentWordElement = document.getElementById('w-' + currentWordIndex);
+}
+
+function incrementLetter(reset) {
+    
+    if (reset) 
+        currentLetterIndex = -1;
+        
+    if (currentLetterElement !== null && currentLetterElement !== undefined) {
+        currentLetterElement.className = typeCorrect;
+    }
+    currentLetterIndex++;
+    
+    currentLetterElement = document.getElementById('w-' + currentWordIndex +' l-' + currentLetterIndex);
+    currentLetterElement.className = typeSelected;
+}
+
+
+
+textBox.addEventListener('keydown', KeyboardEvent => {
+    let key = KeyboardEvent.key;
+
+
+    if (key === currentLetterElement.innerHTML) {
+        if (key === ' ') {
+            incrementWord()
+            incrementLetter(true)
+        } else {
+
+            incrementLetter(false);
+        }
+    }
+});
 
 
 
