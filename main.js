32357ea7c14475fc73db56fadd01eb9e9e46f875
EnderPicture <enderpicture@gmail.com>
2018-09-01 17:34:39 -0700

    CSS Skined and backspace working

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const ID_LETTER = 'letter';
+const ID_WORD = 'word';
+
 let text = '';
 let words = [];
 let textarea = document.getElementById('textarea');
@@ -8,6 +11,10 @@ let currentWordIndex = -1;
 let currentWordElement;
 let currentLetterIndex = -1;
 let currentLetterElement;
+let currentWrongWordIndex = -1;
+let currentWrongWordElement;
+let currentWrongLetterIndex = -1;
+let currentWrongLetterElement;
 
 let wordSelected = 'wordSelected';
 let wordNotSelected = 'wordNotSelected';
@@ -15,6 +22,8 @@ let typeError = 'typeError';
 let typeCorrect = 'typeCorrect';
 let typeSelected = 'typeSelected';
 let typeNotSelected = 'typeNotSlected';
+
+let wordLength = 'data-word-length';
 
 
 function subText() {
@@ -25,19 +34,21 @@ function subText() {
 
     for (let i = 0; i < words.length; i++) {
         let newWord = document.createElement('span');
-        newWord.id = 'w-' + i ;
+        newWord.id = idGen(ID_WORD, i);
         newWord.className = wordNotSelected;
         
-        let letters = (words[i]+' ').split('');
+        let letters = (words[i] + ' ').split('');
+        newWord.setAttribute(wordLength, letters.length);
+
         for (let j = 0; j < letters.length; j++) {
             let newLetter = document.createElement('span');
-            newLetter.id = 'w-' + i+' l-' + j;
+            newLetter.id = idGen(ID_LETTER, i, j);
             newLetter.className = typeNotSelected;
 
             newLetter.innerHTML = letters[j];
             newWord.appendChild(newLetter);
         }
-        
+
         frag.appendChild(newWord);
     }
 
@@ -47,46 +58,120 @@ function subText() {
 
 function start() {
     incrementWord();
-    incrementLetter();
-    
+    incrementLetter(true);
+    incrementWrongLetter(true);
 }
 
 function incrementWord() {
+    if (currentWordElement !== null && currentWordElement !== undefined)
+        currentWordElement.className = wordNotSelected;
     currentWordIndex++;
-    currentWordElement = document.getElementById('w-' + currentWordIndex);
+    currentWordElement = document.getElementById(idGen(ID_WORD, currentWordIndex));
+    currentWordElement.className = wordSelected;
 }
 
 function incrementLetter(reset) {
-    
-    if (reset) 
+
+    if (reset)
         currentLetterIndex = -1;
-        
+
     if (currentLetterElement !== null && currentLetterElement !== undefined) {
         currentLetterElement.className = typeCorrect;
     }
     currentLetterIndex++;
-    
-    currentLetterElement = document.getElementById('w-' + currentWordIndex +' l-' + currentLetterIndex);
+
+    currentLetterElement = document.getElementById(idGen(ID_LETTER, currentWordIndex, currentLetterIndex));
     currentLetterElement.className = typeSelected;
+}
+
+function incrementWrongLetter(reset, backwards) {
+    if (reset) {
+        currentWrongWordIndex = currentWordIndex;
+        currentWrongLetterIndex = currentLetterIndex;
+
+        currentWrongWordElement = document.getElementById(idGen(ID_WORD, currentWrongWordIndex));
+        currentWrongLetterElement = document.getElementById(idGen(ID_LETTER, currentWrongWordIndex, currentWrongLetterIndex));
+        return;
+    }
+
+    let letterNotNull = currentWrongLetterElement !== null && currentWrongLetterElement !== undefined;
+
+    if (!backwards) {
+        
+        if (letterNotNull && currentWrongLetterElement.innerHTML === ' ') {
+            currentWrongWordIndex++;
+            currentWrongWordElement = document.getElementById(idGen(ID_WORD, currentWrongWordIndex));
+            currentWrongLetterIndex = -1;
+        }
+        
+        currentWrongLetterElement.classList.add(typeError);
+        currentWrongLetterIndex++;
+        currentWrongLetterElement = document.getElementById(idGen(ID_LETTER, currentWrongWordIndex, currentWrongLetterIndex));
+    } else {
+        if (letterNotNull && currentWrongLetterIndex === 0) {
+            currentWrongWordIndex--;
+            currentWrongWordElement = document.getElementById(idGen(ID_WORD, currentWrongWordIndex));
+            currentWrongWordElement.getAttribute(wordLength);
+            currentWrongLetterIndex = currentWrongWordElement.getAttribute(wordLength);;
+        }
+        currentWrongLetterIndex--;
+        currentWrongLetterElement = document.getElementById(idGen(ID_LETTER, currentWrongWordIndex, currentWrongLetterIndex));
+        currentWrongLetterElement.classList.remove(typeError);
+    }
+
+
+
+    
+}
+
+function idGen(type, wordIndex, letterIndex) {
+    if (type === 'word') {
+        return 'w-' + wordIndex;
+    } else if (type === 'letter') {
+        return 'w-' + wordIndex + ' l-' + letterIndex;
+    }
 }
 
 
 
-textBox.addEventListener('keydown', KeyboardEvent => {
+textBox.addEventListener('keypress', KeyboardEvent => {
     let key = KeyboardEvent.key;
+    console.log(key);
 
 
-    if (key === currentLetterElement.innerHTML) {
+
+    if (key === currentLetterElement.innerHTML && 
+        currentWrongWordIndex === currentWordIndex && 
+        currentWrongLetterIndex === currentLetterIndex) {
+        
+        
+
         if (key === ' ') {
-            incrementWord()
-            incrementLetter(true)
+            incrementWord();
+            incrementLetter(true);
         } else {
 
             incrementLetter(false);
         }
+
+        incrementWrongLetter(true);
+    } else { 
+        incrementWrongLetter(false, false);
     }
+
 });
 
+textBox.addEventListener('keydown', KeyboardEvent => {
+    let key = KeyboardEvent.key;
+    
+
+    if (key === 'Backspace') {
+        if (currentWrongWordIndex > currentWordIndex ||
+            currentWrongLetterIndex > currentLetterIndex)
+            
+            incrementWrongLetter(false, true);
+    }
+});
 
 
 
